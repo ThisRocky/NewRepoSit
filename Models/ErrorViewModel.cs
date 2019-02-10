2f13d6892dc0e71f65f1f29e5e67aed48de5c5ec
ThisRocky <44053635+ThisRocky@users.noreply.github.com>
2019-02-10 14:22:03 +0100

    FirstCode

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LudoBrowser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
